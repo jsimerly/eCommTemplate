34e6b37241309c007998011546f2030923cd9baf
Jacob <simerly81@gmail.com>
2023-01-31 00:24:08 -0500

    fixed bug where {ref} needed to be {ref()]

--- ecommTemplate/src/components/Searchbar.jsx
+++ ecommTemplate/src/components/Searchbar.jsx
@@ -32,7 +32,7 @@ const Searchbar = () => {
                     <path clipRule="evenodd" fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" />
                 </svg>
         <div className={`bg-white cursor-pointer rounded-md mr-1 min-w-[140px] min-h-[40px] shadow focus-shadow-outline focus:outline-none placeholder-tertiaryTone-200 ${value == "" ? 'text-tertiaryTone-300': 'text-tertiary'} pl-10 p-2`} onClick={onClick} ref={ref} >
-            {ref}
+            {ref()}
             {value == '' ? 'From' : value}
         </div>
         </label>
@@ -47,7 +47,7 @@ const Searchbar = () => {
                 <path clipRule="evenodd" fillRule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" />
             </svg>
             <div className={`bg-white cursor-pointer rounded-md mr-1 min-w-[140px] min-h-[40px] shadow focus-shadow-outline focus:outline-none placeholder-tertiaryTone-200 ${value == "" ? 'text-tertiaryTone-300': 'text-tertiary'} pl-10 p-2`} onClick={onClick} ref={ref} >
-                {ref}
+                {ref()}
                 {value == '' ? 'to' : value}
             </div>
         </label>
