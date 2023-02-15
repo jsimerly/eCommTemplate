5c7534c8ebe52797fb0235922d9f9eb26150f45f
Jacob <simerly81@gmail.com>
2023-02-15 14:39:07 -0500

    still need to make site responsive

--- ecommTemplate/src/components/HeroBanners/Polariod.jsx
+++ ecommTemplate/src/components/HeroBanners/Polariod.jsx
@@ -5,10 +5,10 @@ function Polariod () {
     <div className='inline-flex w-100 cursor-pointer'>
       <img src={polariod_banner} className='relative'/>
       <div className='absolute top-1/2 w-[66%] h-[40%] flex flex-col justify-center items-center my-6'>
-        <h1 className='text-[48px] font-[900] leading-none p-3 br-text-outline-thick text-center'>
+        <h1 className='text-tertiary text-[48px] font-[900] leading-none p-3  text-center'>
           Capture Moments <br/> that will Last Forever
         </h1>
-        <h1 className='text-[30px] br-text-outline-thick underline hover:scale-105'>
+        <h1 className='text-tertiary text-[30px] underline hover:scale-105'>
           Check Out Cameras
         </h1>
       </div>
