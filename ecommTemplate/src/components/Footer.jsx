89bc7e595e2108bb167386a113bf577c1b098b0c
Jacob <simerly81@gmail.com>
2023-04-12 23:05:57 -0400

    footer responsive fixes

--- ecommTemplate/src/components/Footer.jsx
+++ ecommTemplate/src/components/Footer.jsx
@@ -9,9 +9,9 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 const Footer = () => {
   return (
     <div className='flex justify-center w-full bg-primary mt-6'>
-        <div className={`${styles.boxWidth}`}>
+    <div className={`${styles.boxWidth}`}>
     <section className={`flex justify-center items-center sm:py-6 py-16 px-4 sm:px-10 flex-col text-white w-full`}>
-        <div className='grid grid-cols-2 text-center sm:text-start sm:flex flex-row flex-wrap justify-around sm:justify-between items-start w-full sm:py-10' >
+        <div className='flex flex-wrap text-center sm:text-start sm:flex flex-row  justify-around sm:justify-between items-start w-full sm:py-10' >
             {footerInfo.map((footerColInfo, index) => (
                 <div key={index} className='pr-6 pt-6 sm:p-0'>
                     <div className='text-[20px] font-semibold'>
@@ -24,7 +24,7 @@ const Footer = () => {
                     </ul>
                 </div>
             ))}
-            <div className='bg-white rounded-md pt-0 p-10 sm:relative sm:bottom-6 hidden sm:block text-tertiary'>
+            <div className='bg-white rounded-md pt-0 p-10 sm:relative sm:bottom-6  text-tertiary mt-6 sm:mt-0'>
                 <div className='mt-6 font-semibold mb-2'> Stay Connected </div>
                 <div className='flex flex-row justify-start items-center mb-2'>
                     <input placeholder='Email' className='my-2 rounded-md p-2 pl-4 bg-tertiaryTone-100 '/>
@@ -33,25 +33,9 @@ const Footer = () => {
                         content={<ArrowForwardIcon className='group-hover:scale-110'/>}
                     />
                 </div>
-                <div className='text-[12px] max-w-[250px]'>
+                <div className='text-tertiary text-[12px] max-w-[250px]'>
                     The Blue Elf newsletter is only twice monthly and we send special news letter only discounts. We also update you on new products, deals, and expansion cities. 
                 </div>
-            </div>
-        </div>
-        <div className='bg-white rounded-md pt-0 p-10 sm:relative sm:bottom-6 sm:hidden block mt-6 sm:mt-0'>
-            <div className='mt-6 font-semibold mb-2'> Subscribe </div>
-            <div className='flex flex-row justify-start items-center mb-2'>
-                <input placeholder='Email' className='my-2 rounded-md p-2 pl-4 bg-tertiaryTone-100 '/>
-                <div className="ml-1 bg-primary text-white rounded p-2">
-                    <svg 
-                    className='min-w-[22px] min-h-[22px] cursor-pointer'
-                    fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                    </svg>
-                </div>
-            </div>
-            <div className='text-[12px] max-w-[250px]'>
-                The Blue Elf newsletter is only twice monthly and we send special news letter only discounts. We also update you on new products, deals, and expansion cities. 
             </div>
         </div>
         <div className='mt-12 sm:mt-0 p-2 border-t border-white w-full flex sm:justify-between sm:flex-row flex-col justify-center items-center sm:flex-nowrap'>
