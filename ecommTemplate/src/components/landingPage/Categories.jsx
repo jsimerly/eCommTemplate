cf52e36455aad4f572d001b1b499fbebd87a7721
Jacob <simerly81@gmail.com>
2023-04-12 23:13:31 -0400

    landing page full responsive

--- ecommTemplate/src/components/landingPage/Categories.jsx
+++ ecommTemplate/src/components/landingPage/Categories.jsx
@@ -12,11 +12,11 @@ const CatCard = ({header, img, desc, cta1, nav, reverse}) => {
     }
 
     return(
-        <div className={`flex ${reverse ? 'flex-row-reverse' : 'flex-row'} w-full pt-10`}>
-            <img src={img} className='w-3/5 rounded-md'/>
-            <div className={`w-2/5 text-center pb-2 flex flex-col ${reverse ? 'mr-20' : 'ml-20'}`}>
-                <div className='h-full flex flex-col justify-center'>
-                    <h3 className='font-bold text-[40px] text-primary'>
+        <div className={`flex flex-col ${reverse ? 'sm:flex-row-reverse' : 'sm:flex-row'} w-full sm:pt-10`}>
+            <img src={img} className='sm:w-3/5 sm:rounded-md'/>
+            <div className={`sm:w-2/5 text-center pb-2 flex flex-col ${reverse ? 'sm:mr-20' : 'sm:ml-20'}`}>
+                <div className='h-full flex flex-col justify-center mt-4 sm:mt-0 mx-4'>
+                    <h3 className='font-bold sm:text-[40px] text-[30px] text-primary'>
                         {header}
                     </h3>
                     <div className='text-tertiary text-[18px]'>
@@ -42,7 +42,7 @@ const Categories = () => {
         <div className='flex justify-center items-center text-[36px] text-center font-bold text-tertiary pb-6 mx-3'>
             {categories.title}
         </div>
-        <div className='flex flex-col gap-[300px] pb-[100px]'>
+        <div className='flex flex-col gap-10 sm:gap-[300px] sm:pb-[100px]'>
             {categories.cats.map((cat, index) => {
                 const reverse = index % 2 === 1
                 return (
