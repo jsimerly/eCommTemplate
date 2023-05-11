5b19b2711ddbaa4eb74e33780d81d0e6b5da5835
Jacob <simerly81@gmail.com>
2023-05-10 22:06:51 -0400

    working full, just need to hide 8000 port

--- api/api/settings.py
+++ api/api/settings.py
@@ -20,9 +20,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
@@ -31,6 +31,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ['DEBUG'] == 'True'
 ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = '/api/static'
 
 # Application definition
 
