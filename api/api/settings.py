f3eb761e91173f0f1ea5176ea8d5b42978936bec
Jacob <simerly81@gmail.com>
2023-05-15 12:01:02 -0400

    some settings changes

--- api/api/settings.py
+++ api/api/settings.py
@@ -58,11 +58,7 @@ INSTALLED_APPS = [
 ]
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1",
-]
+CORS_ALLOWED_ORIGINS = os.environ['ALLOWED_HOSTS'].split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
