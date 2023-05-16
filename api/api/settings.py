7a351bcb08771318c7aa5292184986cff38b8598
Jacob <simerly81@gmail.com>
2023-05-16 12:25:14 -0400

    cors updated

--- api/api/settings.py
+++ api/api/settings.py
@@ -60,8 +60,10 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8080",
-    "http://127.0.0.1:5173",
     "http://127.0.0.1",
+    'http://3.16.250.50',
+    'http://rentblueelf.com',
+    "http://www.rentblueelf.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
