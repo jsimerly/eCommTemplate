e9fd7aa986393159d596f9b2cadb7fb54dd248a5
Jacob <simerly81@gmail.com>
2023-05-18 10:43:45 -0400

    update to frontend

--- frontend/src/api/serverConstants.js
+++ frontend/src/api/serverConstants.js
@@ -1,1 +1,1 @@
-export const SERVER_ADDRESS =    'https://www.rentblueelf.com' // 'http://127.0.0.1' //
+export const SERVER_ADDRESS =   'https://www.rentblueelf.com' // 'http://127.0.0.1:8000' //  
